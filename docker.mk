88161452558e4505f366abd4369b8ec1a366b410
Dorian Drivet <doriandrivet@icloud.com>
2024-12-11 10:23:59 +0100

    Initial commit - Setup project structure

---
+++ docker.mk
@@ -0,0 +1,8 @@
+#######################################
+# docker.mk
+# Options pour l'environnement Docker
+#######################################
+CC=gcc
+LIBSLOCAL=-L/usr/lib -llapack -lblas -lm
+INCLUDEBLASLOCAL=-I/usr/include
+OPTCLOCAL=-fPIC -march=native 
